2af877ca186351fad5b7016c8ee052023ec2773c
Luis Tripa <54379665+luistripa@users.noreply.github.com>
2020-03-26 12:08:36 +0000

    Created Main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.utils.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        //TODO: Everything else
+
+        in.close();
+    }
+}
