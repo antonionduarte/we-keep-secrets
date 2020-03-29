9adb7dea3683101eb202344417d1f5973d4c4616
Luis Tripa <54379665+luistripa@users.noreply.github.com>
2020-03-29 21:19:58 +0100

    Added a constants and an incomplete command processor to main.java

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,57 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        // Command constants
+        public static final String EXIT = "EXIT";
+        public static final String HELP = "HELP";
+        public static final String REGISTER = "REGISTER";
+        public static final String LISTUSERS = "LISTUSERS";
+        public static final String UPLOAD = "UPLOAD";
+        public static final String WRITE = "WRITE";
+        public static final String READ = "READ";
+        public static final String GRANT = "GRANT";
+        public static final String REVOKE = "REVOKE";
+        public static final String USERDOCS = "USERDOCS";
+        public static final String TOPLEAKED = "TOPLEAKED";
+        public static final String TOPGRANTERS = "TOPGRANTERS";
 
-        //TODO: Everything else
+        // System out messages
+        // TODO
+
+        Scanner in = new Scanner(System.in);
+        String command = "";
+
+        while (!command.equals(EXIT)) {
+            command = getCommand(in);
+            processCommand(command);
+        }
 
         in.close();
+    }
+
+    private static processCommand(Scanner in, String command) {
+
+        switch (command) {
+
+            case EXIT: {
+                System.out.println(EXIT_MESSAGE);
+                break;
+            }
+            case HELP: {
+                processHelp();
+                break;
+            }
+        }
+    }
+
+    private static void processHelp() {
+        // TODO
+    }
+
+
+
+
+    private static String getCommand(Scanner in) {
+        return in.nextLine().toUpperCase();
     }
 }
