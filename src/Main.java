796e7361afe6240a85a358bc49c4f2e9d496a4c6
Luis Tripa <54379665+luistripa@users.noreply.github.com>
2020-03-31 18:15:05 +0100

    Updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,57 +2,70 @@ import java.utils.Scanner;
 
 public class Main {
 
+    // Command constants
+    public static final String REGISTER = "REGISTER";
+    public static final String LISTUSERS = "LISTUSERS";
+    public static final String UPLOAD = "UPLOAD";
+    public static final String WRITE = "WRITE";
+    public static final String READ = "READ";
+    public static final String GRANT = "GRANT";
+    public static final String REVOKE = "REVOKE";
+    public static final String USERDOCS = "USERDOCS";
+    public static final String TOPLEAKED = "TOPLEAKED";
+    public static final String TOPGRANTERS = "TOPGRANTERS";
+    public static final String HELP = "HELP";
+    public static final String EXIT = "EXIT";
+
+    // System out messages
+    private static final String EXIT_MESSAGE = "Bye!";
+    // TODO
+
     public static void main(String[] args) {
 
-        // Command constants
-        public static final String EXIT = "EXIT";
-        public static final String HELP = "HELP";
-        public static final String REGISTER = "REGISTER";
-        public static final String LISTUSERS = "LISTUSERS";
-        public static final String UPLOAD = "UPLOAD";
-        public static final String WRITE = "WRITE";
-        public static final String READ = "READ";
-        public static final String GRANT = "GRANT";
-        public static final String REVOKE = "REVOKE";
-        public static final String USERDOCS = "USERDOCS";
-        public static final String TOPLEAKED = "TOPLEAKED";
-        public static final String TOPGRANTERS = "TOPGRANTERS";
-
-        // System out messages
-        // TODO
-
+        // Initiate Scanner
         Scanner in = new Scanner(System.in);
+
+        // Initiate DocumentCollection and UserCollection
+        DocumentCollection dc = new DocumentCollectionClass();
+        UserCollection uc = new UserCollectionClass();
+
+        // Other instance variables
         String command = "";
 
+        // Main loop
         while (!command.equals(EXIT)) {
             command = getCommand(in);
-            processCommand(command);
+            processCommand(in, uc, dc, command);
         }
 
+        // Close Scanner and terminate (I'll be back)
         in.close();
     }
 
-    private static processCommand(Scanner in, String command) {
+    private static processCommand(Scanner in, UserCollection uc, DocumentCollection dc, String command) {
 
         switch (command) {
-
-            case EXIT: {
+            case REGISTER:
+                processRegister(in, uc, dc);
+                break;
+            // TODO: Add the other commands
+            case EXIT:
                 System.out.println(EXIT_MESSAGE);
                 break;
-            }
-            case HELP: {
+            case HELP:
                 processHelp();
                 break;
-            }
         }
     }
 
-    private static void processHelp() {
+    private static void processRegister(Scanner in, UserCollection uc, DocumentCollection dc) {
         // TODO
     }
 
-
-
+    private static void processHelp() {
+        for(HelpMenu h: HelpMenu.values())
+            System.out.println(h.message());
+    }
 
     private static String getCommand(Scanner in) {
         return in.nextLine().toUpperCase();
