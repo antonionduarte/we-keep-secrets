027b06d2bd457540b47481dfe371fb28bc93e8fb
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-04-03 20:04:28 +0100

    Added new comments.

--- src/documents/ClassifiedDocumentClass.java
+++ src/documents/ClassifiedDocumentClass.java
@@ -4,6 +4,13 @@ import users.*;
 
 public class ClassifiedDocumentClass extends DocumentAbstractClass {
 
+    
+    /**
+     * Constructor of the Classified Documents.
+     * @param ID that identifies the document.
+     * @param description or content that the document has.
+     * @param manager is the user that created the document.
+     */
     public ClassifiedDocumentClass(String ID, String description, User manager) {
         super(ID, description, manager);
     }
