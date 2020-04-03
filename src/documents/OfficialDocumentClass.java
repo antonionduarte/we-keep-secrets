a8d1e6cb04d4e8010e2988a0a039a4c2d521ac77
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-04-03 19:53:16 +0100

    Update OfficialDocumentClass.java

--- src/documents/OfficialDocumentClass.java
+++ src/documents/OfficialDocumentClass.java
@@ -4,6 +4,12 @@ import users.*;
 
 public class OfficialDocumentClass extends DocumentAbstractClass {
 
+    /**
+     * Constructor of the Official Documents.
+     * @param ID that identifies the document.
+     * @param description or content that the document has.
+     * @param manager is the user that created the document.
+     */
     public OfficialDocumentClass(String ID, String description, User manager) {
         super(ID, description, manager);
     }
