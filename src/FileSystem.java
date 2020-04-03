d1486854b442d3ecf75c6188230875ceb083bcbd
António Duarte <antonioduarte@antonio-duarte.lan>
2020-04-03 02:26:47 +0100

    Changed the class and interface names

--- src/FileSystem.java
+++ src/FileSystem.java
@@ -1,4 +1,4 @@
 
-public interface System {
+public interface FileSystem {
 
 }
--- src/FileSystemClass.java
+++ src/FileSystemClass.java
@@ -1,4 +1,4 @@
 
-public class SystemClass implements System {
+public class FileSystemClass implements FileSystem {
 
 }
