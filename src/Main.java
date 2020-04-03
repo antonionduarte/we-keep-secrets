68e10fabc2d0b26ef80cc546767c1d404c7aa59e
Luis Tripa <54379665+luistripa@users.noreply.github.com>
2020-04-03 22:13:16 +0100

    Minor fixes to main
    Enhanced readability in constants and fixed the processRegister method wich contained too many params

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,18 @@ import users.*;
 public class Main {
 
     // Command constants
-    public static final String REGISTER = "REGISTER";
-    public static final String LISTUSERS = "LISTUSERS";
-    public static final String UPLOAD = "UPLOAD";
-    public static final String WRITE = "WRITE";
-    public static final String READ = "READ";
-    public static final String GRANT = "GRANT";
-    public static final String REVOKE = "REVOKE";
-    public static final String USERDOCS = "USERDOCS";
-    public static final String TOPLEAKED = "TOPLEAKED";
-    public static final String TOPGRANTERS = "TOPGRANTERS";
-    public static final String HELP = "HELP";
-    public static final String EXIT = "EXIT";
+    public static final String REGISTER     = "REGISTER";
+    public static final String LISTUSERS    = "LISTUSERS";
+    public static final String UPLOAD       = "UPLOAD";
+    public static final String WRITE        = "WRITE";
+    public static final String READ         = "READ";
+    public static final String GRANT        = "GRANT";
+    public static final String REVOKE       = "REVOKE";
+    public static final String USERDOCS     = "USERDOCS";
+    public static final String TOPLEAKED    = "TOPLEAKED";
+    public static final String TOPGRANTERS  = "TOPGRANTERS";
+    public static final String HELP         = "HELP";
+    public static final String EXIT         = "EXIT";
 
     // System out messages
     private static final String EXIT_MESSAGE = "Bye!";
@@ -53,7 +53,7 @@ public class Main {
         }
     }
 
-    private static void processRegister(Scanner in, UserCollection uc, DocumentCollection dc) {
+    private static void processRegister(Scanner in) {
         // TODO
     }
 
