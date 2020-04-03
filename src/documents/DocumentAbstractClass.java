665bf609ce773a49925f059e331805e397142c56
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-04-03 19:07:15 +0100

    Implemented basic methods.

--- src/documents/DocumentAbstractClass.java
+++ src/documents/DocumentAbstractClass.java
@@ -1,8 +1,49 @@
 package documents;
 
+import users.*;
+
 public abstract class DocumentAbstractClass implements Document {
 
-    protected DocumentAbstractClass() {
+    // Constants
 
+    // Variables
+
+    /**
+     * The description (one can even say content) of the document.
+     */
+    String description;
+
+    /**
+     * The identifier of the document.
+     */
+    String ID;
+
+    /**
+     * The manager of the document (user who created it).
+     */
+    User manager;
+
+    /**
+     * The constructor of the document.
+     * @param ID that identifies the document.
+     * @param description or content that the document has.
+     * @param manager is the user that created the document.
+     */
+    protected DocumentAbstractClass(String ID, String description, User manager) {
+        this.ID = ID;
+        this.description = description;
+        this.manager = manager;
+    }
+
+    // Methods
+
+    @Override
+    public void setDescription(String text) {
+        this.description = text;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
     }
 }
