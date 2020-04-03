67df8bd5703a40810af357b5ac8a7ffbd42969dd
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-04-03 19:34:57 +0100

    Changed the add methods to two.

--- src/documents/DocumentCollection.java
+++ src/documents/DocumentCollection.java
@@ -1,7 +1,11 @@
 package documents;
 
+import users.*;
+
 public interface DocumentCollection {
 
-    
-    
+    void addOfficialDocument(String ID, String description, User manager);
+
+    void addClassifiedDocument(String ID, String description, User manager);
+
 }
