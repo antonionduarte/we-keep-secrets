ea8852ec2fd809567ab78265706edd2fd849115f
António Duarte <antonioduarte@antonio-duarte.lan>
2020-04-03 09:43:16 +0100

    Implemented correct interface.

--- src/users/UserCollectionClass.java
+++ src/users/UserCollectionClass.java
@@ -2,14 +2,14 @@ package users;
 
 import iterators.*;
 
-public class UserCollectionClass implements UserCollectionClass {
+public class UserCollectionClass implements UserCollection {
 
     // Generic Constants
     private static final int DEFAULT_VECTOR_SIZE = 50;
     private static final int GROWTH_FACTOR = 2;
 
     // Instance variables
-    User users;
+    User[] users;
     int userCounter;
 
     /**
