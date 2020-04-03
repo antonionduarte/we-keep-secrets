860758031fd99f4aa0891c22189167313cf4c4ac
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-04-03 19:50:12 +0100

    Fixed the comments

--- src/documents/DocumentCollection.java
+++ src/documents/DocumentCollection.java
@@ -4,8 +4,20 @@ import users.*;
 
 public interface DocumentCollection {
 
+    /**
+     * Adds an official document to the collection.
+     * @param ID that identifies the document.
+     * @param description or content that the document has.
+     * @param manager is the user that created the document.
+     */
     void addOfficialDocument(String ID, String description, User manager);
 
+    /**
+     * Adds a classified document to the collection.
+     * @param ID that identifies the document.
+     * @param description or content that the document has.
+     * @param manager is the user that created the document.
+     */
     void addClassifiedDocument(String ID, String description, User manager);
 
 }
