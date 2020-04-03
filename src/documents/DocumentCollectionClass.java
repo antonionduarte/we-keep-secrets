7b08573d06b96c52ec4f7f7a63b37cc71881c293
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-04-03 19:07:50 +0100

    Added basic implementation.

--- src/documents/DocumentCollectionClass.java
+++ src/documents/DocumentCollectionClass.java
@@ -2,7 +2,30 @@ package documents;
 
 public class DocumentCollectionClass implements DocumentCollection {
 
-    public DocumentCollectionClass() {
+    // Constants
+    private static final int DEFAULT_SIZE = 100;
+    private static final int GROWTH_FACTOR = 2;
 
+    // Variables
+
+    /**
+     * The array of documents.
+     */
+    private Document[] documents;
+
+    /**
+     * The counter associated with the document array <code>documents</code>.
+     */
+    private int counter;
+
+    public DocumentCollectionClass() {
+        this.documents = new Document[DEFAULT_SIZE];
     }
+
+    // Methods
+
+    private void resize() {
+        Document[] tempDocuments;
+    }
+
 }
