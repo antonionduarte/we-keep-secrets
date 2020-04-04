cd7ba323c0a7b3048a210907ce838e28c272ba73
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-04-04 19:36:36 +0100

    Added pre conditions to the comments

--- src/documents/Document.java
+++ src/documents/Document.java
@@ -1,22 +1,27 @@
 package documents;
 
+import users.*;
+
 public interface Document {
 
     /**
      * Sets a new description to the document.
      * @param text to set the description to.
+     * PRE: text != NULL
      */
     void setDescription(String text);
 
     /**
      * Shows the description of the document.
      * @return <code>description</code> of the document.
+     * PRE: description != NULL
      */
     String getDescription();
 
     /**
      * Shows the ID of the document.
      * @return the <code>ID</code> of the document.
+     * PRE ID != NULL
      */
     String getID();
 
