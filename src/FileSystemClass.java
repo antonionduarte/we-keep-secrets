7fb5de721017a4d5b3c59f1e761dd043550fde7f
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-04-08 21:54:51 +0100

    Started working on createDocument method

--- src/FileSystemClass.java
+++ src/FileSystemClass.java
@@ -5,11 +5,34 @@ import clearance.*;
 
 public class FileSystemClass implements FileSystem {
 
-    DocumentCollection dc;
-    UserCollection uc;
+    User managerLol;
+
+    DocumentCollection documentCollection;
+    UserCollection userCollection;
 
     public FileSystemClass() {
-        dc = new DocumentCollectionClass();
-        uc = new UserCollectionClass();
+        documentCollection = new DocumentCollectionClass();
+        userCollection = new UserCollectionClass();
+    }
+
+    /**
+     * (TEMP) upload <doc_name> <user_id> <security_level> <description>
+     * TODO: replace managerLol lmao.
+     */
+    public void createDocument(String ID, String managerID, String clearance, String description) {
+
+        User manager = userCollection.searchIndex(managerID);
+
+        if (clearance.equals(Clearance.CLERK.getClearanceString())) {
+            OfficialDocument document = new OfficialDocumentClass(ID, description, managerLol, Clearance.CLERK);
+        } else if (clearance.equals(Clearance.CONFIDENTIAL.getClearanceString())) {
+            ClassifiedDocument document = new ClassifiedDocumentClass(ID, description, managerLol, Clearance.CONFIDENTIAL);
+        } else if (clearance.equals(Clearance.SECRET.getClearanceString())) {
+            ClassifiedDocument document = new ClassifiedDocumentClass(ID, description, managerLol, Clearance.SECRET);
+        } else {
+            ClassifiedDocument document = new ClassifiedDocumentClass(ID, description, managerLol, Clearance.TOPSECRET);
+        }
+        
+        documentCollection.addDocument(document);
     }
 }
