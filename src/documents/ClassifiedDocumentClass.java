34ee5a702e71548ff08fe23eed08086a2f3efc30
Luis Tripa <54379665+luistripa@users.noreply.github.com>
2020-04-09 12:21:51 +0100

    Added hasGrant() and searchIndexOfGrant() to the document class

--- src/documents/ClassifiedDocumentClass.java
+++ src/documents/ClassifiedDocumentClass.java
@@ -26,5 +26,17 @@ public class ClassifiedDocumentClass extends AbstractDocument implements Classif
         grants.addUser(user);
     }
 
+    public boolean hasGrant(User user) {
+        return searchIndexOfGrant(user);
+    }
+
+    private int searchIndexOfGrant(String userID) {
+        int pos = -1;
+        for (int i=0 ; i<userCounter&&pos==-1 ; i++) {
+            if (grants[i].getID().equals(userID))
+                pos = i;
+        }
+        return pos;
+    }
     
 }
