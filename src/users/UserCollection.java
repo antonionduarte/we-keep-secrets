4f206af061636c0443657aeff3bb60a3363d5896
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-04-14 19:37:32 +0100

    Minor refactor to the comments and fixed some typos

--- src/users/UserCollection.java
+++ src/users/UserCollection.java
@@ -33,88 +33,72 @@ public interface UserCollection {
 
 	/**
 	 * Gets the user id as it is stored in the array.
-	 *
-	 * @param      userID  The user id
-	 *
-	 * @return     The user id stored in the array.
+	 * @param userID  The user id.
+	 * @return the user id stored in the array.
 	 */
 	String getUserID(String userID);
 
 	/**
 	 * Gets the user kind.
-	 *
-	 * @param      userID  The user id
-	 *
-	 * @return     The user kind.
+	 * @param userID  The user id.
+	 * @return the user kind.
 	 */
     String getUserKind(String userID);
 
     /**
      * Gets the user clearance.
-     *
-     * @param      userID  The user id
-     *
-     * @return     The user clearance.
+     * @param userID  the user id.
+     * @return the user clearance.
      */
     Clearance getUserClearance(String userID);
 
     /**
      * Gets the document clearance.
-     *
-     * @param      managerID   The manager id
-     * @param      documentID  The document id
-     *
-     * @return     The document clearance.
+     * @param managerID   The manager id.
+     * @param documentID  The document id.
+     * @return the document clearance.
      */
     Clearance getDocumentClearance(String managerID, String documentID);
 
     /**
      * Checks if user is the manager of a given document.
-     *
-     * @param      userID      The user id
-     * @param      documentID  The document id
-     *
-     * @return     True if is manager, False otherwise
+     * @param userID the user id.
+     * @param documentID the document id.
+     * @return true if is manager, false otherwise.
      */
     boolean userHasDocument(String userID, String documentID);
 
     /**
-     * Uploads a document to the user's document collection. The user will becomem the manager of this document.
-     *
-     * @param      userID       The user id
-     * @param      documentID   The document id
-     * @param      description  The document's description
-     * @param      clearance    The document's clearance
+     * Uploads a document to the user's document collection. The user will become the manager of this document.
+     * @param userID the user id.
+     * @param documentID the document id.
+     * @param description the document's description.
+     * @param clearance the document's clearance.
      */
     void upload(String userID, String documentID, String description, Clearance clearance);
 
     /**
-     * Reads the <code>description</code> of a document owned by a user
-     *
-     * @param      managerID   The manager id
-     * @param      documentID  The document id
-     *
-     * @return     { description_of_the_return_value }
+     * Reads the <code>description</code> of a document owned by a user.
+     * @param managerID the manager id.
+     * @param documentID the document id.
+     * @return TODO: { description_of_the_return_value }
      */
     String read(String managerID, String documentID);
 
     /**
-     * Generates a user's document iterator
-     *
-     * @param      userID  The user id
-     *
-     * @return     an iterator object
+     * Generates a user's document iterator.
+     * @param userID the user id.
+     * @return an iterator object.
      */
     Iterator<Document> userDocs(String userID);
 
     /**
-     * Writes <code>description</code> to a specific document
-     *
-     * @param      managerID   The manager id
-     * @param      userID      The user id requesting the write
-     * @param      documentID  The document id
-     * @param      decription  The new decription
+     * Writes <code>description</code> to a specific document.
+     * @param managerID the manager id.
+     * @param userID the user id requesting the write.
+     * @param documentID the document id.
+     * @param description the new decription.
      */
-    void write(String managerID, String userID, String documentID, String decription);
+    void write(String managerID, String userID, String documentID, String description);
 
 }
