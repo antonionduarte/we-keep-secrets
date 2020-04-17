ba410d0a9a0d2fd5dbef8ad0f095f1a2a076460f
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-04-17 11:15:31 +0100

    Revert "Changed a comment"
    This reverts commit 242ae4055d050ea2ada67c21aaba14e338a5f2e1.

--- src/actions/Action.java
+++ src/actions/Action.java
@@ -4,13 +4,13 @@ import users.User;
 
 public interface Action {
 
-	/**
-	 * In the Actions what happens is that we are storing a User that the action is related with and it's type.
-	 * 
-	 * In the Write and Read actions the related user is the one who read or wrote on the document.
-	 * 
-	 * In the Grant and Revoke actions the related user is the one who got the Document granted
-	 * or revoked from them. (This is also the basis of our grant system).
+	/*
+	  In the Actions what happens is that we are storing a User that the action is related with and it's type.
+
+	  In the Write and Read actions the related user is the one who read or wrote on the document.
+
+	  In the Grant and Revoke actions the related user is the one who got the Document granted
+	  or revoked from them. (This is also the basis of our grant system).
 	 */
 
 	/**
@@ -18,7 +18,7 @@ public interface Action {
 	 * In case of Grant or Revoke actions it's the User that the Document will be Granted to or Revoked from.
 	 * In case of Read or Write actions it's the User that read or wrote to the document.
 	 * @return the User that is related to the action.
-	 */    
+	 */
 	User getRelatedUser();
 
 	/**
@@ -26,15 +26,4 @@ public interface Action {
 	 * @return the type of the action (read, write, grant, revoke).
 	 */
 	Actions getActionType();
-
-	/**
-	 * 
-	 */
-	void revoke();
-
-	/**
-	 * Checks if a User has been revoked a Document.
-	 * @return true if it has been revoked, false if otherwise.
-	 */
-	boolean isRevoked();
 }
