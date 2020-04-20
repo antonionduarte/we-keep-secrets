eb0f5786bdb70eed6502939eb58569491d943628
Luis Tripa <54379665+luistripa@users.noreply.github.com>
2020-04-20 10:17:01 +0100

    Updated TestParser

--- src/TestParser.py
+++ src/TestParser.py
@@ -20,12 +20,19 @@ TEST_LIST = [
 
 NUMBER_OF_TESTS = 15
 
+system("javac -Xlint Main.java")
+
 COMMAND_RUN = "java Main < ../WeKeepSecretsTests/%.2d_in_%s.txt > ../tests/%.2d_%s_test_out.txt"
 
 for i in range(0, NUMBER_OF_TESTS):
 	system(COMMAND_RUN % (i+1, TEST_LIST[i], i+1, TEST_LIST[i]))
-	print(COMMAND_RUN % (i+1, TEST_LIST[i], i+1, TEST_LIST[i]))
 	code = system("diff ../tests/%.2d_%s_test_out.txt ../WeKeepSecretsTests/%.2d_out_%s.txt" % (i+1, TEST_LIST[i], i+1, TEST_LIST[i]))
 	if code != 0:
-		print("Failed test %s!" % str(i+1))
+		print("------------------------")
+		print("       FAILED %.2s        " % str(i+1))
+		print("------------------------")
 		exit(0)
+	else:
+		print("------------------------")
+		print("       PASSED %.2s        " % str(i+1))
+		print("------------------------")
